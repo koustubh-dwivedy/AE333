dc23a86c83146944b18bd9ac3cc3164f013d204a
Koustubh <koustubhdwivedy@gmail.com>
2016-09-12 02:57:16 +0530

    final version

--- assignment_1/sim_driver.m
+++ assignment_1/sim_driver.m
@@ -1,26 +1,33 @@
-angle_of_attack = 0;
-v_inf = 15;
-num_panels = 38;
+c_l = zeros(50, 1);
+c_d = zeros(50, 1);
+for t = 1:50
+    angle_of_attack = -30;
+    v_inf = 15*abs(cos(t*4*pi/50)); % 2 Time Periods
+    num_panels = 38;
 
-[x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end] = panels(num_panels);
+    [x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end] = panels(num_panels);
 
-[x_vals_start, y_vals_start] = rotation(x_vals_start, y_vals_start, angle_of_attack);
-[x_vals_centre, y_vals_centre] = rotation(x_vals_centre, y_vals_centre, angle_of_attack);
-[x_vals_end, y_vals_end] = rotation(x_vals_end, y_vals_end, angle_of_attack);
+    [x_vals_start, y_vals_start] = rotation(x_vals_start, y_vals_start, angle_of_attack);
+    [x_vals_centre, y_vals_centre] = rotation(x_vals_centre, y_vals_centre, angle_of_attack);
+    [x_vals_end, y_vals_end] = rotation(x_vals_end, y_vals_end, angle_of_attack);
 
-beta_i = panel_normals(x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end);
+    beta_i = panel_normals(x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end);
 
-plot([x_vals_start, x_vals_start(1, 1)], [y_vals_start, y_vals_start(1, 1)]);
-axis equal;
+%     plot([x_vals_start, x_vals_start(1, 1)], [y_vals_start, y_vals_start(1, 1)]);
+%     axis equal;
 
-I_mat = I_matrix(num_panels, x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end);
+    I_mat = I_matrix(num_panels, x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end);
 
-lambda = solver(v_inf, beta_i, I_mat);
-%for checking accuracy of solution->
-sum(lambda.*(sqrt((x_vals_end' - x_vals_start').*(x_vals_end' - x_vals_start') + (y_vals_end' - y_vals_start').*(y_vals_end' - y_vals_start'))))
+    lambda = solver(v_inf, beta_i, I_mat);
+    %for checking accuracy of solution->
+%     sum(lambda.*(sqrt((x_vals_end' - x_vals_start').*(x_vals_end' - x_vals_start') + (y_vals_end' - y_vals_start').*(y_vals_end' - y_vals_start'))))
 
-I_mat_new = I_matrix_new(num_panels, x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end);
-c_p_i = pressure_coeff(lambda, I_mat_new, v_inf, beta_i);
+    I_mat_new = I_matrix_new(num_panels, x_vals_start, y_vals_start, x_vals_centre, y_vals_centre, x_vals_end, y_vals_end);
+    c_p_i = pressure_coeff(lambda, I_mat_new, v_inf, beta_i);
 
-[y_dummy, domain_start, domain_end] = shape(0, 1);
-[c_l, c_d] = lift_drag_coeff(beta_i, c_p_i, (domain_end - domain_start), x_vals_start, y_vals_start, x_vals_end, y_vals_end);
+    [y_dummy, domain_start, domain_end] = shape(0, 1);
+    [c_l(t), c_d(t)] = lift_drag_coeff(beta_i, c_p_i, (domain_end - domain_start), x_vals_start, y_vals_start, x_vals_end, y_vals_end);
+    % plot([x_vals_centre'; x_vals_centre(1)], [c_p_i', c_p_i(1)]);
+end
+plot([1:50], c_l);
+plot([1:50], c_d);
